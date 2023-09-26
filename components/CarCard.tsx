1aa7eb67a357f66105a74c80d326fe267e275207
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-09-26 00:49:16 -0500

    card details modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@
 import { CarProps } from '@/types';
 import { calculateCarRent } from '@/utils';
 import Image from 'next/image';
-import { CustomButton } from '.';
+import { CarDetails, CustomButton } from '.';
 import { useState } from 'react';
 
 interface CarCardProps {
@@ -77,6 +77,12 @@ const CarCard = ({ car }: CarCardProps) => {
 					/>
 				</div>
 			</div>
+
+			<CarDetails
+				isOpen={isOpen}
+				closeModal={() => setIsOpen(false)}
+				car={car}
+			/>
 		</div>
 	);
 };
