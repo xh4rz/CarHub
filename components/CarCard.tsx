75980b417f73b8979b654ff7cddd3c93186d96f6
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-09-26 01:21:21 -0500

    car images

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { CarProps } from '@/types';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import Image from 'next/image';
 import { CarDetails, CustomButton } from '.';
 import { useState } from 'react';
@@ -33,7 +33,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
 			<div className="relative w-full h-40 my-3 object-contain">
 				<Image
-					src="/hero.png"
+					src={generateCarImageUrl(car)}
 					alt="car model"
 					fill
 					priority
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types';
+
 export async function fetchCars() {
 	const headers = {
 		'X-RapidAPI-Key': '339e58a16dmshb2aace9f9a3f754p11d4bajsn1d239028f985',
@@ -5,7 +7,7 @@ export async function fetchCars() {
 	};
 
 	const response = await fetch(
-		'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+		'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',
 		{
 			headers: headers
 		}
@@ -29,4 +31,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 	const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
 	return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+	const url = new URL('https://cdn.imagin.studio/getimage');
+
+	const { make, year, model } = car;
+
+	url.searchParams.append('customer', 'hrjavascript-mastery');
+	url.searchParams.append('make', make);
+	url.searchParams.append('modelFamily', model.split(' ')[0]);
+	url.searchParams.append('zoomType', 'fullscreen');
+	url.searchParams.append('modelYear', `${year}`);
+	url.searchParams.append('angle', `${angle}`);
+
+	return `${url}`;
 };
