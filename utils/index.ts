8ebb3bcdedd5fdd6f2d9bea245f9d222dab5b7b7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-09-27 15:12:52 -0500

    show more

--- utils/index.ts
+++ utils/index.ts
@@ -9,7 +9,7 @@ export async function fetchCars(filters: FilterProps) {
 	};
 
 	const response = await fetch(
-		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=${model}&limit=${limit}&fuel_type=${fuel}`,
+		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
 		{
 			headers: headers
 		}
